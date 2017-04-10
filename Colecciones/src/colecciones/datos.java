81d5bfb32eb14a7a18faa97693c14bffc9ee42cc
julio <julio@julio-ThinkPad-R400>
2017-04-10 17:47:29 -0600

    agregue las clases necesarias para trabajar con cadenas

---
+++ Colecciones/src/colecciones/datos.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colecciones;
+import java.util.Vector;
+import java.util.StringTokenizer;
+import java.util.StringJoiner;
+/**
+ *
+ * @author julio
+ */
+public class datos {
+    Vector<String> datos = new Vector <String>();
+    
+    public void llenarVector(String dato){
+       StringTokenizer obj = new StringTokenizer(dato);
+       
+    }
+    public void vericarPalabras(){
+        //aqui va todo el codigo para saber si las palabras se repiten
+        
+    }
+    public void mostrarVectores(){
+        
+    }
+}
